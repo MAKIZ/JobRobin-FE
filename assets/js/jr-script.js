5888e78bf5bb65c86eb73d16b9e057f8acfe370c
Mark Delos Reyes <makizworks@gmail.com>
2017-10-23 11:35:45 -0400

    update buttons and navbar

--- assets/js/jr-script.js
+++ assets/js/jr-script.js
@@ -1,1 +1,23 @@
-console.log("jr script in!")
+var navTop = $("#home").offset().top;
+var header = $("#header").offset().top;
+var role = $("#role").offset().top;
+var feedback = $("#feedback").offset().top;
+var profile = $("#profile").offset().top;
+var globe = $("#globe").offset().top;
+
+$(document).scroll(function() {
+    var scrollPosition = $(document).scrollTop();
+    if(scrollPosition >= navTop && scrollPosition < header) {
+        $("#mainNav").css("background", "rgba(0, 0, 0, 0)");
+    } else if(scrollPosition >= header && scrollPosition < role) {
+        $("#mainNav").css("background", "#49556C");
+    } else if(scrollPosition >= role && scrollPosition < feedback) {
+        $("#mainNav").css("background", "#00BDB1");
+    } else if(scrollPosition >= feedback && scrollPosition < profile) {
+        $("#mainNav").css("background", "#49556C");
+    } else if(scrollPosition >= profile && scrollPosition < globe) {
+        $("#mainNav").css("background", "#FF9A2A");
+    } else if(scrollPosition >= globe) {
+        $("#mainNav").css("background", "#49556C");
+    }
+})
