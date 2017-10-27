248b0ee0d0c4f17b667a1c98b7af3fdb4acd14d2
Mark Delos Reyes <makizworks@gmail.com>
2017-10-27 00:04:42 -0400

    update text employer page

--- assets/js/jr-script-emp.js
+++ assets/js/jr-script-emp.js
@@ -6,7 +6,7 @@ var pricing = $("#pricing").offset().top - 66;
 var whywork = $("#whywork").offset().top;
 
 if ($(window).width() < 992) {
-    $("#mainNav").css("background", "#49556C");
+    $("#mainNav").css("background", "#00BDB1");
 } else {
     navBkgEmp();
 }
